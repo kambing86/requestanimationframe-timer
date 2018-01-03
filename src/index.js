0c5d2097a7fcb990cd666c866c6a88061df73d00
Chua Kang Ming <kambing860210@gmail.com>
2018-01-03 15:02:13 +0800

    first commit

---
+++ src/index.js
@@ -0,0 +1,84 @@
+const raf = require('raf');
+
+const MODE_TIMEOUT = 0;
+const MODE_INTERVAL = 1;
+const fnStacks = new Map();
+const runArray = new Set();
+let rafStarted = false;
+let startId = 0;
+
+function getTimeStamp() {
+  return new Date().getTime();
+}
+
+function addToRun(id) {
+  const value = fnStacks.get(id);
+  runArray.add(value);
+}
+
+function runFunction() {
+  if (runArray.size === 0) return;
+  runArray.forEach((value) => {
+    const { fn, args } = value;
+    fn(...args);
+  });
+  runArray.clear();
+}
+
+function loop() {
+  const currentTimeTick = getTimeStamp();
+  fnStacks.forEach((value, id) => {
+    const { nextTick, ms, mode } = value;
+    if (currentTimeTick - nextTick >= 0) {
+      addToRun(id);
+      if (mode === MODE_TIMEOUT) {
+        fnStacks.delete(id);
+      } else {
+        fnStacks.set(id, {
+          ...value,
+          nextTick: nextTick + ms,
+        });
+      }
+    }
+  });
+  runFunction();
+  if (fnStacks.size === 0) {
+    rafStarted = false;
+    return;
+  }
+  raf(loop);
+}
+
+function addId({ fn, ms = 0, args, mode }) {
+  if (!fn) return null;
+  const currentId = startId;
+  fnStacks.set(currentId, {
+    fn,
+    ms,
+    nextTick: getTimeStamp() + ms,
+    args,
+    mode,
+  });
+  if (!rafStarted) {
+    rafStarted = true;
+    raf(loop);
+  }
+  startId += 1;
+  return currentId;
+}
+
+function removeId(id) {
+  if (fnStacks.has(id)) {
+    fnStacks.delete(id);
+  }
+  if (fnStacks.size === 0) {
+    rafStarted = false;
+  }
+}
+
+export default {
+  setTimeout: (fn, ms = 0, ...args) => addId({ fn, ms, args, mode: MODE_TIMEOUT }),
+  clearTimeout: removeId,
+  setInterval: (fn, ms = 0, ...args) => addId({ fn, ms, args, mode: MODE_INTERVAL }),
+  clearInterval: removeId,
+};
